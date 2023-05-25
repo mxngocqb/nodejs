1e738c817287113fd0c52cbe5b41a190eda4e93d
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-05-25 07:34:03 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -5,24 +5,29 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
 // var assetsPath = path.join(__dirname, 'public');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine
-app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: ".hbs" }));
+app.engine(
+    'hbs',
+    handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// Route init
+route(app);
 
-app.get('/new', (req, res) => {
-    res.render('new');
-})
-
-app.listen(port, () => console.log(`Example app listening at ${port}`))
+app.listen(port, () => console.log(`Example app listening at ${port}`));
